faad96cc05d6f4356351297ed8f557a68245bf36
Robert Rakhmatullin <robertbg4@gmail.com>
2020-12-01 16:33:47 +0800

    gunicorn added

--- config.py
+++ config.py
@@ -9,12 +9,13 @@ def get_required_env(var_name):
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TELEGRAM_BOT_TOKEN = get_required_env("TELEGRAM_BOT_TOKEN")
-CHAT_ID = get_required_env("CHAT_ID")
+
 DRCHRONO_CLIENT_ID = get_required_env("DRCHRONO_CLIENT_ID")
 DRCHRONO_CLIENT_SECRET = get_required_env("DRCHRONO_CLIENT_SECRET")
 DRCHRONO_REFRESH_TOKEN = get_required_env("DRCHRONO_REFRESH_TOKEN")
+
 SECRET_KEY = get_required_env("SECRET_KEY")
+
 DOCTOR_ID = get_required_env("DOCTOR_ID")
 OFFICE_ID = get_required_env("OFFICE_ID")
 EXAM_ROOM = get_required_env("EXAM_ROOM")
